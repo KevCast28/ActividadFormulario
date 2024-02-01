e9fb4030cdc12f7a4f021ac27ab5eba07e5b8652
KevCast28 <kevinsn524@gmail.com>
2024-01-31 19:40:01 -0500

    modified:   src/views/HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@ import type { Ref } from 'vue';
   const age:Ref<number> = ref(0);
   const sex:Ref<string[]> = ref(['Masculino', 'Femenino', 'Otro']);
   const selectedSex:Ref<string> = ref('');
+  const customGender:Ref<string> = ref('');
 
   let nameError = '';
   let lastNameError = '';
@@ -36,12 +37,8 @@ function validateAge() {
   }
 }
 
-function validateSex() {
-  if (selectedSex.value === 'Otro') {
-    sexError = 'Por favor, especifica el género';
-  } else {
-    sexError = '';
-  }
+function handleSexChange() {
+  sexError = '';
 }
 
 function submitForm() {
@@ -51,35 +48,46 @@ function submitForm() {
   }
 }
 
-function hasErrors() {
-  return nameError || lastNameError || ageError || sexError;
+function hasErrors(): boolean {
+    return !!nameError || !!lastNameError || !!ageError || !!sexError;
 }
 
 </script>
 
 <template>
-  <div>
-    <input v-model="name" type="text" @input="validateName">
+  <div class="form-container">
+    <h1>Formulario</h1>
+    <div class="form-field">
+    <label for="name">Nombre</label>
+    <input id="name" v-model="name" type="text" @input="validateName">
     <span v-if="nameError" class="error">{{ nameError }}</span>
   </div>
   <div>
-    <input v-model="lastName" type="text" @input="validateLastName">
+    <label for="name">Apellido</label>
+    <input id="last-name" v-model="lastName" type="text" @input="validateLastName">
     <span v-if="lastNameError" class="error">{{ lastNameError }}</span>
   </div>
   <div>
-    <input v-model="age" type="number" @input="validateAge">
+    <label for="name">Edad</label>
+    <input id="age" v-model="age" type="number" @input="validateAge">
     <span v-if="ageError" class="error">{{ ageError }}</span>
   </div>
   <div>
-    <select v-model="selectedSex" @change="validateSex">
+    <label for="name">Género</label>
+    <select id="sex" v-model="selectedSex" @change="handleSexChange">
       <option value="Masculino">Masculino</option>
       <option value="Femenino">Femenino</option>
       <option value="Otro">Otro</option>
     </select>
     <span v-if="sexError" class="error">{{ sexError }}</span>
   </div>
+  <div v-if="selectedSex === 'Otro'" class="form-field">
+      <label for="custom-gender">Especificar género</label>
+      <input id="custom-gender" v-model="customGender" type="text">
+    </div>
   <div>
-    <button @click="submitForm" :disabled="hasErrors('')">Enviar</button>
+    <button @click="submitForm" :disabled="hasErrors()">Enviar</button>
+  </div>
   </div>
 </template>
 
@@ -88,6 +96,24 @@ function hasErrors() {
   div {
     margin-bottom: 10px;
   }
+
+  .form-container {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+}
+
+.form-field {
+  margin-bottom: 15px;
+}
+
+label {
+  font-weight: bold;
+  margin-right: 10px;
+}
 
   /* Estilos para los campos de entrada */
   input[type="text"],
